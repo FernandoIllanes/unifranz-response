ff3f604d24aceddbfce9f7942c466b43b7260b09
FernandoIllanes <lpze.alvarofernando.illanes.ca@unifranz.edu.bo>
2024-05-31 15:02:01 -0400

    baileys-version

--- index.js
+++ index.js
@@ -1,101 +1,142 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const {
+    default: makeWASocket,
+    DisconnectReason,
+    useMultiFileAuthState,
+} = require('@whiskeysockets/baileys');
+
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const http = require('http');
-const port = 3001;
+const socketIO = require('socket.io');
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    webVersionCache: {
-        type: "remote",
-        remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-    },
+const app = express();
+app.use(fileUpload({ createParentPath: true }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/assets', express.static(__dirname + '/client/assets'));
+
+const server = http.createServer(app);
+const io = socketIO(server);
+const port = process.env.PORT || 8000;
+
+app.get('/scan', (req, res) => {
+    res.sendFile('./client/index.html', { root: __dirname });
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
+app.get('/', (req, res) => {
+    res.send('server working');
 });
 
-client.on('ready', () => {
-    console.log('Cliente listo!');
+let sock;
+let qrDinamic;
+let soket;
 
-    /* client.getChats().then(chats => {
-        const groups = chats.filter(chat => chat.isGroup);
-        groups.forEach(group => {
-            console.log(`Grupo: ${group.name} - ID: ${group.id._serialized}`);
-        });
-    }).catch(err => {
-        console.error('Error listando los grupos:', err);
-    }); */
-
-    setInterval(async () => {
-        try {
-            const info = await client.info;
-            const phoneNumber = info.me.user;
-            await client.sendMessage(`${phoneNumber}@c.us`, "Ping para mantener la sesión activa (solo para pruebas)");
-            console.log("Ping enviado");
-        } catch (error) {
-            console.error("Error al enviar el ping:", error);
-        }
-    }, 12 * 60 * 60 * 1000);
-});
-
-client.on('auth_failure', (msg) => {
-    console.error('Fallo al autenticar', msg);
-});
-
-client.initialize();
-
-// Función para realizar el reemplazo de variables
-function buildMessageTemplate(template, values) {
-    return template.replace(/{([^{}]*)}/g, (match, key) => {
-        return values[key] || match;
+async function connectToWhatsApp() {
+    const { state, saveCreds } = await useMultiFileAuthState('session_auth_info');
+    sock = makeWASocket({
+        auth: state,
+        version: [2, 2413, 1]
     });
+
+    sock.ev.on('connection.update', async (update) => {
+        const { connection, lastDisconnect, qr } = update;
+        qrDinamic = qr;
+        if (connection === 'close') {
+            let reason = new DisconnectReason(lastDisconnect.error).toString();
+            if (reason === DisconnectReason.badSession) {
+                console.log(`Bad Session File, Please Delete session_auth_info and Scan Again`);
+                sock.logout();
+            } else if ([DisconnectReason.connectionClosed, DisconnectReason.connectionLost, DisconnectReason.connectionReplaced, DisconnectReason.loggedOut, DisconnectReason.restartRequired, DisconnectReason.timedOut].includes(reason)) {
+                console.log(`Connection closed, reconnecting due to reason: ${reason}`);
+                connectToWhatsApp();
+            } else {
+                console.log(`Unknown disconnect reason: ${reason}`);
+            }
+        } else if (connection === 'open') {
+            console.log('Connection open');
+            updateQR('connected');
+        }
+    });
+
+    sock.ev.on('messages.upsert', async ({ messages, type }) => {
+        if (type === 'notify') {
+            console.log('Mensaje entrante recibido');
+        }
+    });
+
+    sock.ev.on('creds.update', saveCreds);
 }
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/send-message') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
+const isConnected = () => {
+    return !!sock?.user;
+};
 
-        req.on('end', async () => {
-            try {
-                const reqData = JSON.parse(body);
-                let contactId;
+function buildMessageTemplate(template, data) {
+    return template.replace(/{(\w+)}/g, (_, key) => data[key] || '');
+}
 
-                if (reqData.contact_type === 'group') {
-                    // es un grupo
-                    contactId = reqData.contact_id + '@g.us';
-                } else if (reqData.contact_type === 'contact') {
-                    // es un contacto
-                    contactId = reqData.contact_id.replace(/\+/g, "") + '@c.us';
-                }
+app.post('/send-message', async (req, res) => {
+    console.log('Hola desde el send-message');
+    try {
+        const reqData = req.body;
+        let contactId;
 
-                if (reqData.message_type === 'static') {
-                    await client.sendMessage(contactId, reqData.message);
-                } else if (reqData.message_type === 'customized') {
-                    const message = buildMessageTemplate(reqData.message_template, reqData);
-                    await client.sendMessage(contactId, message);
-                } else {
-                    res.writeHead(400, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ status: 'error', message: 'Tipo de mensaje no valido' }));
-                }
+        if (reqData.contact_type === 'group') {
+            contactId = reqData.contact_id + '@g.us';
+        } else if (reqData.contact_type === 'contact') {
+            contactId = reqData.contact_id.replace(/\+/g, "") + '@s.whatsapp.net';
+        }
 
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ status: 'error', message: 'Mensaje enviado correctamente' }));
-            } catch (error) {
-                console.error('Error procesando la solicitud:', error);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ status: 'error', message: 'Error procesando la solicitud' }));
-            }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ status: 'error', message: 'URL no encontrada' }));
+        let messageOptions;
+        if (reqData.message_type === 'static') {
+            messageOptions = { text: reqData.message };
+        } else if (reqData.message_type === 'customized') {
+            const message = buildMessageTemplate(reqData.message_template, reqData);
+            messageOptions = { text: message };
+        } else {
+            res.status(400).json({ status: 'error', message: 'Tipo de mensaje no válido' });
+            return;
+        }
+
+        await sock.sendMessage(contactId, messageOptions);
+
+        res.status(200).json({ status: 'success', message: 'Mensaje enviado correctamente' });
+    } catch (error) {
+        console.error('Error procesando la solicitud:', error);
+        res.status(500).json({ status: 'error', message: 'Error procesando la solicitud' });
     }
 });
 
+io.on('connection', (socket) => {
+    soket = socket;
+    if (isConnected()) {
+        updateQR('connected');
+    } else if (qrDinamic) {
+        updateQR('qr');
+    }
+});
+
+const updateQR = (data) => {
+    if (data === 'qr') {
+        qrcode.toDataURL(qrDinamic, (err, url) => {
+            soket?.emit('qr', url);
+            soket?.emit('log', 'QR recibido, escanear con WhatsApp');
+        });
+    } else if (data === 'connected') {
+        soket?.emit('qrstatus', './assets/check.svg');
+        soket?.emit('log', 'Usuario conectado');
+        const { id, name } = sock?.user;
+        soket?.emit('user', `${id} ${name}`);
+    } else if (data === 'loading') {
+        soket?.emit('qrstatus', './assets/loader.gif');
+        soket?.emit('log', 'Cargando...');
+    }
+};
+
+connectToWhatsApp().catch((err) => console.log('Error inesperado: ' + err));
 server.listen(port, () => {
-    console.log('Servidor escuchando en http://localhost:3001');
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
